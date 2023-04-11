ea0c92cbdb4268801909ef74a310125e863cb626
Eniko Galambos <eniko.galambos@wundermanthompson.com>
2023-04-11 10:34:05 +0200

    Add search bar

--- src/pages/List/List.tsx
+++ src/pages/List/List.tsx
@@ -1,5 +1,59 @@
+import { Container, IconButton, InputBase, Paper, Box } from '@mui/material';
+import IconSearch from '@mui/icons-material/Search';
+import { KeyboardEvent, useState } from 'react';
+
 const List = () => {
-  return <p>Hello</p>;
+  const [searchKeyCurrentValue, setSearchKeyCurrentValue] = useState('');
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setSearchKeyCurrentValue(event.target.value);
+  };
+
+  const handleSubmit = (): void => {
+    // TODO searchKeyCurrentValue
+  };
+
+  return (
+    <Container maxWidth="md" sx={{ mt: 5, mb: 5 }}>
+      <Box component="form">
+        <Paper
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            width: {
+              xs: '100%',
+              md: '300px',
+            },
+          }}
+        >
+          <InputBase
+            sx={{ ml: 1.5, flex: 1 }}
+            placeholder="Search"
+            inputProps={{
+              'aria-label': 'Search',
+            }}
+            onKeyUp={(e: KeyboardEvent<HTMLInputElement>) => {
+              if (e.key === 'Enter') {
+                e.preventDefault();
+                handleSubmit();
+              }
+            }}
+            onChange={handleChange}
+            value={searchKeyCurrentValue}
+          />
+          <IconButton
+            type="button"
+            sx={{ p: '10px' }}
+            aria-label="Search"
+            onClick={handleSubmit}
+            color="primary"
+          >
+            <IconSearch />
+          </IconButton>
+        </Paper>
+      </Box>
+    </Container>
+  );
 };
 
 export default List;
